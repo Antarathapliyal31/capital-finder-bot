a4dfd3d6cb0d7985a77799b7b174812930d558c7
Antarathapliyal31 <antara31work@gmail.com>
2025-07-07 14:02:51 -0400

    Update capital_bot.py

--- capital_bot.py
+++ capital_bot.py
@@ -1,18 +1,24 @@
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import Ollama
+import streamlit as st
 
+st.title("Capital Finding Bot")
+user_input=st.text_input("Enter a country name ")
 prompt=PromptTemplate.from_template("What is the capital of {country} ?")
 #temperature controls how random or creative the model’s responses are.
 #0-Very focused, deterministic (same input = same output every time)
 #0.5-Balanced — some creativity, still consistent
 #0.8+-Very creative/random — great for storytelling or brainstorming
 # Ollama = A free app to run powerful AI chatbots directly on your laptop. No internet, no API key, no cost.
-llm = Ollama(model="mistral", temperature=0,num_predict=26)
-#Earlier I was not using temperature and num_predict parameters, it was taking 5.30 seconds, after using them it reduced the response time to 1.26 seconds)
-#num_predict parameter limits how many tokens (words or word-parts) the model is allowed to generate
-#less tokens= faster response
+llm = Ollama(model="mistral", temperature=0, num_predict=26)
 chain = prompt | llm
-user_input=input("Enter a country name")
-response=chain.invoke({"country":user_input})
-print("The answer to your question is",response)
+if st.button("Find the Capital"):
+    if user_input:
+        response=chain.invoke({"country":user_input})
+        st.success(response)
+    else:
+        st.warning("Please enter a country name")
+
+
+
 
