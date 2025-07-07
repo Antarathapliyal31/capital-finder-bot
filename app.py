9d16c944c0d985301cc20bd34ade873c12a9d672
Antarathapliyal31 <antara31work@gmail.com>
2025-07-07 14:38:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from langchain.prompts import PromptTemplate
-from langchain_ollama import OllamaLLM
+from langchain_openai import ChatOpenAi
+import os
 import streamlit as st
 st.title("Capital Finding Bot")
+api=st.text_input("enter your OpenAI API key", type="password")
 user_input=st.text_input("Enter a country name ")
 prompt=PromptTemplate.from_template("What is the capital of {country} ?")
 
@@ -10,14 +12,19 @@ prompt=PromptTemplate.from_template("What is the capital of {country} ?")
 #0.5-Balanced — some creativity, still consistent
 #0.8+-Very creative/random — great for storytelling or brainstorming
 # Ollama = A free app to run powerful AI chatbots directly on your laptop. No internet, no API key, no cost.
-llm = OllamaLLM(model="mistral", temperature=0, num_predict=20)
-chain = prompt | llm
-if st.button("Find the Capital"):
-    if user_input:
+
+if api and user_input:
+    os.environ["OPENAI_API_KEY"] = api
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, num_predict=20)
+    chain=prompt | llm
+    if st.button("Find the Capital"):
         response=chain.invoke({"country":user_input})
         st.success(response)
     else:
         st.warning("Please enter a country name")
+else:
+    st.warning("Please enter a valid OpenAI API key")
+
 
 
 
