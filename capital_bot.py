013ceee21acb08e8a37e7815592f4d8372c429ca
Antarathapliyal31 <antara31work@gmail.com>
2025-07-07 13:28:38 -0400

    Update capital_bot.py

--- capital_bot.py
+++ capital_bot.py
@@ -7,7 +7,8 @@ prompt=PromptTemplate.from_template("What is the capital of {country} ?")
 #0.5-Balanced — some creativity, still consistent
 #0.8+-Very creative/random — great for storytelling or brainstorming
 # Ollama = A free app to run powerful AI chatbots directly on your laptop. No internet, no API key, no cost.
-llm = Ollama(model="mistral")
+llm = Ollama(model="mistral", temperature=0,num_predict=26)
+#Earlier I was not using temperature and num_predict parameters, it was taking 5.30 seconds, after using them it reduced the response time to 1.26 seconds)
 chain = prompt | llm
 user_input=input("Enter a country name")
 response=chain.invoke({"country":user_input})
